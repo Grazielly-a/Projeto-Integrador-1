7d678a835215cd890512d9ca385349060446add0
Grazielly Araujo <grazielly.araujo@outlook.com>
2022-08-03 10:42:34 -0300

    Criação da Categoria Model e Banco de Dados

---
+++ acquacode/src/main/java/com/generation/acquacode/model/Categoria.java
@@ -0,0 +1,53 @@
+package com.generation.acquacode.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "Obrigatório inserir o tipo de categoria")
+	@Size(min = 2, max = 50)
+	private String tipo;
+	
+	@NotBlank(message = "Obrigatório inserir o tipo de categoria")
+	@Size(min = 2, max = 50)
+	private String marca;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	
+
+}
